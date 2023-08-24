1b6bf2a04a7c0ce27695bdcc3cf491d15650e7c1
Grzegorz <grzegorz.skrabucha@gmail.com>
2023-08-24 20:50:51 +0200

    Icons added

--- src/components/templates/Header/index.tsx
+++ src/components/templates/Header/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {GiHamburgerMenu as HamburgerMenuIcon} from 'react-icons/gi'
+import {BsHeart as HeartIcon} from 'react-icons/bs'
+import {AiOutlineShoppingCart as CartIcon} from 'react-icons/ai'
 type Props = {}
 
 const Header = (props: Props) => {
@@ -7,8 +9,8 @@ const Header = (props: Props) => {
     <header className='fixed left-0 w-full top-0 h-[100px] flex justify-between items-center px-[20px]  backdrop-blur'>
         <HamburgerMenuIcon className='w-[27px] h-[27px]' />
         <div className='flex items-center space-x-[14px]'>
-            <div className='bg-red-500 w-[24px] h-[24px]'></div>
-            <div className='bg-red-500 w-[24px] h-[24px]'></div>
+            <HeartIcon size='30'/>
+            <CartIcon size='30'/>
             <div className='bg-red-500 w-[24px] h-[24px]'></div>
         </div>
     </header>
